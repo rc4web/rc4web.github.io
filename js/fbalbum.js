13532b491dd2c4f30f133c766cba46e9f1c3cff0
reyy <rey@live.jp>
2015-05-24 17:29:41 +0800

    Integrated fb album to index page

---
+++ js/fbalbum.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".fb-album-container").FacebookAlbumBrowser({
+        account: "NUSResidentialCollege4",
+        skipAlbums: ["Profile Pictures", "Timeline Photos", "Cover Photos"],
+        showAccountInfo: false,
+        showImageCount: false,
+        showImageText: true,
+        lightbox: true,
+        photosCheckbox: false,
+        photoChecked: function(photo){
+            console.log("PHOTO CHECKED: " + photo.id + " - " + photo.url + " - " + photo.thumb);
+            console.log("CHECKED PHOTOS COUNT: " + this.checkedPhotos.length);
+        },
+        photoUnchecked: function (photo) {
+            console.log("PHOTO UNCHECKED: " + photo.id + " - " + photo.url + " - " + photo.thumb);
+            console.log("CHECKED PHOTOS COUNT: " + this.checkedPhotos.length);
+        },
+        albumSelected: function (photo) {
+            console.log("ALBUM CLICK: " + photo.id + " - " + photo.url + " - " + photo.thumb);
+        },
+        photoSelected: function (photo) {
+            console.log("PHOTO CLICK: " + photo.id + " - " + photo.url + " - " + photo.thumb);
+        }
+    });
+});
